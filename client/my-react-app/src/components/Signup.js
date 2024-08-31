fb54e2e93a34300e318e37484aad301b2d731ef1
lojank <lojankarunakaran@gmail.com>
2024-08-30 22:07:52 -0400

    Created react form for login and signup pages

--- client/my-react-app/src/components/Signup.js
+++ client/my-react-app/src/components/Signup.js
@@ -1,42 +1,82 @@
 import './Signup.css';
+import {useState} from 'react';
 
 function Signup() {
+
+  const [formData, setFormData] = useState({first_name:"", last_name:"", email: "",password: "", confirm_password:""});
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (formData.password !== formData.confirm_password) {
+      alert('Passwords do not match')
+    } else {
+      alert(`First Name: ${formData.first_name}, Last Name: ${formData.last_name}, Email: ${formData.email}, Password: ${formData.password}, Confirm Password: ${formData.confirm_password}`)
+    
+    }
+    ;
+  };
+
   return (
     <div className="App">
       <img className="icon" src="https://cdn-icons-png.flaticon.com/512/566/566985.png"/>
-      <div className="login-con">
+    <form className="login-con" onSubmit={handleSubmit}>
       <h1 className="loginAcc">Create an Account</h1>
         
     <div className='names'>
       <input
+        id="first_name" 
+        name="first_name"
         type="text"
         placeholder="First Name"
         className="Name-input"
+        value={formData.first_name} 
+        onChange={handleChange}
         />
         <input
+        id="last_name" 
+        name="last_name"
         type="text"
         placeholder="Last Name"
         className="Name-input"
+        value={formData.last_name} 
+        onChange={handleChange}
         />
     </div>
         <input
-        type="text"
+        id="email" 
+        name="email"
+        type="email"
         placeholder="Email"
         className="Email-input"
+        value={formData.email} 
+        onChange={handleChange}
         />
         <input
-        type="text"
+        id="password" 
+        name="password"
+        type="password"
         placeholder="Password"
         className="Email-input"
+        value={formData.password} 
+        onChange={handleChange}
         />
         <input
-        type="text"
+        id="confirm_password" 
+        name="confirm_password"
+        type="password"
         placeholder="Confirm Password"
         className="Email-input"
+        value={formData.confirm_password} 
+        onChange={handleChange}
         />
-        <button className="signInButton">Sign In</button>
+        <button type='submit'className="signInButton">Sign In</button>
 
-      </div>
+      </form>
       <div className="login-img">
        
           <h1 className='new'>Already Have An Account?</h1>
